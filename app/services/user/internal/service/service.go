34d0b31cebb27a444c140c96a9a284f99ee554e7
AndroSaal <89251409398@mail.ru>
2024-12-19 11:57:54 +0300

    update service level

--- app/services/user/internal/service/service.go
+++ app/services/user/internal/service/service.go
@@ -1,3 +1,31 @@
 package service
 
-type Service interface{}
+import "github.com/AndroSaal/RecommendationsForUsers/app/services/user/internal/entities"
+
+type Service interface {
+	UserCreator
+	UserGetter
+	UserUpdator
+	MailSender
+}
+
+type UserCreator interface {
+	CreateUser(user entities.UserInfo) (entities.UserId, error)
+}
+
+type UserGetter interface {
+	GetUserInfoById(id entities.UserId) (entities.UserInfo, error)
+	GetUserByEmail(email entities.Email) (entities.UserInfo, error)
+}
+
+type UserUpdator interface {
+	UpdateUser(user entities.UserInfo) error
+}
+
+type MailSender interface {
+	SendMail(email *entities.Email) error
+}
+
+func NewService() Service{
+	return NewUserService()
+}
---
+++ app/services/user/internal/service/user-service.go
@@ -0,0 +1,30 @@
+package service
+
+import "github.com/AndroSaal/RecommendationsForUsers/app/services/user/internal/entities"
+
+// имплементация интерфейса Service
+type UserService struct{}
+
+func NewUserService() *UserService {
+	return &UserService{}
+}
+
+func (s *UserService) CreateUser(user entities.UserInfo) (entities.UserId, error) {
+	return 0, nil
+}
+
+func (s *UserService) GetUserInfoById(id entities.UserId) (entities.UserInfo, error) {
+	return entities.UserInfo{}, nil
+}
+
+func (s *UserService) GetUserByEmail(email entities.Email) (entities.UserInfo, error) {
+	return entities.UserInfo{}, nil
+}
+
+func (s *UserService) UpdateUser(user entities.UserInfo) error {
+	return nil
+}
+
+func (s *UserService) SendMail(email *entities.Email) error {
+	return nil
+}
